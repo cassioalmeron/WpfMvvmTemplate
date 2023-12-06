932ef917020375966fb685a06b39992873f39416
Cassio Almeron <50529323+cassioalmeron@users.noreply.github.com>
2023-12-06 14:04:30 -0300

    First Commit

---
+++ WpfExample/MVVM/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfExample.MVVM
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string? _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value, () => Name); }
+        }
+    }
+}
