1cedd96c86a73be8c6f44666a08e5fa34005ea9b
Cassio Almeron <50529323+cassioalmeron@users.noreply.github.com>
2023-12-08 18:33:35 -0300

    Add Behavior and RelayCommand
    Add OpenFileBehavior, as a default behavior for opening files.
    Add the RelayCommand as an implementation of ICommand.
    And apply them to the user interface.

--- WpfExample/MVVM/MainViewModel.cs
+++ WpfExample/MVVM/MainViewModel.cs
@@ -1,12 +1,33 @@
-﻿namespace WpfExample.MVVM
+﻿using System.Windows.Input;
+
+namespace WpfExample.MVVM
 {
     public class MainViewModel : ViewModelBase
     {
+        //public MainViewModel()
+        //{
+        //    SelectFileCommand = new RelayCommand(SelectFile);
+        //}
+
         private string? _name;
         public string Name
         {
             get { return _name; }
             set { SetProperty(ref _name, value, () => Name); }
         }
+
+        private string? _fileName;
+        public string FileName
+        {
+            get { return _fileName; }
+            set { SetProperty(ref _fileName, value, () => FileName); }
+        }
+
+        //public ICommand SelectFileCommand { get; private set; }
+
+        //private void SelectFile()
+        //{
+
+        //}
     }
 }
